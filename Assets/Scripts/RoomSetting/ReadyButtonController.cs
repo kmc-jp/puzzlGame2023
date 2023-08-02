825b8889b98abbdea2696b2bf1e0d2a62b7f2abe
LosenkaNiya <lyvolricosmate.creation@gmail.com>
2023-08-02 17:02:20 +0900

    (#32) 準備完了ボタンを押すとCmdChangeReadyStateが呼ばれるが、その挙動を少し変更。

--- Assets/Scripts/RoomSetting/ReadyButtonController.cs
+++ Assets/Scripts/RoomSetting/ReadyButtonController.cs
@@ -1,13 +1,30 @@
+using System;
 using Mirror;
 using UnityEngine;
 
 namespace RoomSetting {
 
     public class ReadyButtonController : MonoBehaviour {
-        public NetworkRoomPlayer LocalPlayer { get; set; }
+        // NOTE:
+        // This field is referred by CustomRoomNetworkManager, which will be unnecessary in the future.
+        // However, because CustomRoomNetworkManager is in the charge of #27 (Here is #32), deletion of it and LocalPlayer are pending.
+        [Obsolete]
+        public NetworkRoomPlayer LocalPlayer;
 
-        public void SendReadyStateToServer() {
-            LocalPlayer.CmdChangeReadyState(true);
+        // This wrapper ensures that CallCmdChangeReadyState is called once per connection. 
+        public void SendReadyToServer() {
+            CallCmdChangeReadyState(true);
+            HideButton();
+        }
+
+        void CallCmdChangeReadyState(bool readyState) {
+            GameObject roomPlayer = NetworkClient.localPlayer.gameObject;
+            NetworkRoomPlayer roomPlayerNetworkRoomPlayer = roomPlayer.GetComponent<NetworkRoomPlayer>();
+            roomPlayerNetworkRoomPlayer.CmdChangeReadyState(readyState);
+        }
+
+        void HideButton() {
+            gameObject.SetActive(false);
         }
     }
 }
