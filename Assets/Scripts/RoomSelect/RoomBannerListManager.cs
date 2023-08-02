d9803c9c025e9aa1a6302724b702d825ebbe1247
LosenkaNiya <lyvolricosmate.creation@gmail.com>
2023-08-02 12:03:42 +0900

    (#27) NetworkDiscoveryの情報をもとに部屋バナーの追加と削除、及び更新を行うRoomBannerListManagerを作成。 バナー追加時に参加ボタンにConnectの処理を割り当てる作業は未着手。

--- Assets/Scripts/RoomSelect/RoomBannerListManager.cs
+++ Assets/Scripts/RoomSelect/RoomBannerListManager.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using Mirror.Discovery;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RoomBannerListManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace RoomSelect {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public class RoomBannerListManager : MonoBehaviour {
+        [SerializeField] private GameObject _roomBannerPrefab;
+        private Transform _roomBannersParent;
+
+        public void UpdateBanner(ServerResponse info) {
+            string address = info.EndPoint.Address.ToString();
+            DeleteBannerifExist(address);
+            CreateBanner(address);
+        }
+
+        void Start() {
+            _roomBannersParent = transform.Find("RoomBannersParent");
+        }
+
+        void CreateBanner(string dedicatedServerOrHostAddress) {
+            GameObject roomBanner = Instantiate(_roomBannerPrefab);
+            roomBanner.transform.SetParent(_roomBannersParent);
+
+            GameObject address = roomBanner.transform.Find("Address").gameObject;
+            Text addressText = address.GetComponent<Text>();
+            addressText.text = dedicatedServerOrHostAddress;
+
+            GameObject joinButton = roomBanner.transform.Find("JoinButton").gameObject;
+            Button joinButtonButton = joinButton.GetComponent<Button>();
+            // joinButtonButton.onClick.AddListener();
+        }
+
+        void DeleteBannerifExist(string dedicatedServerOrHostAddress) {
+            for (int i = 0; i <  _roomBannersParent.childCount; i++) {
+                GameObject roomBanner = _roomBannersParent.GetChild(i).gameObject;
+
+                GameObject address = roomBanner.transform.Find("Address").gameObject;
+                Text addressText = address.GetComponent<Text>();
+
+                if (addressText.text == dedicatedServerOrHostAddress) {
+                    Destroy(roomBanner);
+                    return;
+                }
+            }
+        }
     }
 }
