7aa52344c0ee59fe81cb9470d0095feb8856a331
waiyangar572 <t572waimath1729@gmail.com>
2023-09-27 00:35:04 +0900

    (#57) applyPhisicsParametersを追加

--- Assets/Scripts/PurpleAnimaMovementBehaviour.cs
+++ Assets/Scripts/PurpleAnimaMovementBehaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PurpleAnimaMovementBehaviour : AnimaObject
 {
     [SerializeField] Color Color;
+    [SerializeField] PhysicsMaterial2D Material;
 
     public override Color GetColor()
     {
@@ -18,5 +19,13 @@ public class PurpleAnimaMovementBehaviour : AnimaObject
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void ApplyPhisicsParameters()
+    {
+        var rb = this.GetComponent<Rigidbody2D>();
+
+        rb.sharedMaterial = Material;
+
     }
 }
