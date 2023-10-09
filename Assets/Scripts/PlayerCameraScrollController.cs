07d14e05e1e7f5037fc8416df1daf8b6cc7de562
Niya <133580812+LosenkaNiya@users.noreply.github.com>
2023-10-09 18:15:56 +0900

    81 camera scroll control (#82)
    * (#81)
    スクロールテストシーン作成
    InputManagerを設定
    
    * (#81)
    カメラに取り付けるとスクロール可能なコンポーネントを作成
    
    * (#81)
    自陣・敵陣に素早くカメラを移動するフォーカス機能を追加
    
    * (#81)
    テストシーン削除
    
    * (#81) レビュー対応。Focus機能の修正
    
    ---------
    
    Co-authored-by: Zudies <leon.han@live.com>

---
+++ Assets/Scripts/PlayerCameraScrollController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Camera))]
+internal sealed class PlayerCameraScrollController : MonoBehaviour {
+    [SerializeField] private Rect _scrollableArea = new Rect(-20f, -5f, 40f, 10f);
+    [SerializeField] private float _moveSpeedMaltiply = 0.1f;
+    [SerializeField] private Vector3 _ownHalfFocusPoint = new Vector3(-10f, 0f);
+    [SerializeField] private Vector3 _oppositionHalfFocusPoint = new Vector3(10f, 0f);
+    [SerializeField] private float _focusSpeedMultiply = 1f;
+
+    private Camera _playerCamera;
+
+    private void Awake() {
+        _playerCamera = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        _ownHalfFocusPoint.z = transform.position.z;
+        _oppositionHalfFocusPoint.z = transform.position.z;
+    }
+
+    private void Update() {
+        MoveWithinScrollableArea();
+        FocusOwnOrOppositionHalf();
+    }
+
+    private void MoveWithinScrollableArea() {
+        float horizontalDelta = Input.GetAxis("PlayerCameraHorizontal");
+        float verticalDelta = Input.GetAxis("PlayerCameraVertical");
+        var scrollDelta = new Vector2(horizontalDelta, verticalDelta);
+        scrollDelta *= _moveSpeedMaltiply;
+        transform.Translate(scrollDelta);
+
+        var cameraLeftCenterPoint = _playerCamera.ViewportToWorldPoint(new Vector3(0f, 0.5f, 0f));
+        var cameraRightCenterPoint = _playerCamera.ViewportToWorldPoint(new Vector3(1f, 0.5f, 0f));
+        var cameraBottomCenterPoint = _playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0f, 0f));
+        var cameraTopCenterPoint = _playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 1f, 0f));
+
+        float correctionX = 0f;
+        float correctionY = 0f;
+        if (cameraLeftCenterPoint.x < _scrollableArea.xMin) {
+            correctionX = _scrollableArea.xMin - cameraLeftCenterPoint.x;
+        } else if (cameraRightCenterPoint.x > _scrollableArea.xMax) {
+            correctionX = _scrollableArea.xMax - cameraRightCenterPoint.x;
+        }
+        if (cameraBottomCenterPoint.y < _scrollableArea.yMin) {
+            correctionY = _scrollableArea.yMin - cameraBottomCenterPoint.y;
+        } else if (cameraTopCenterPoint.y > _scrollableArea.yMax) {
+            correctionY = _scrollableArea.yMax - cameraTopCenterPoint.y;
+        }
+
+        var correctionDelta = new Vector2(correctionX, correctionY);
+        transform.Translate(correctionDelta);
+    }
+
+    private void FocusOwnOrOppositionHalf() {
+        if (Input.GetButton("PlayerCameraFocusOwnHalf")) {
+            transform.position = Vector3.MoveTowards(transform.position, _ownHalfFocusPoint, _focusSpeedMultiply);
+        }
+        if (Input.GetButton("PlayerCameraFocusOppositionHalf")) {
+            transform.position = Vector3.MoveTowards(transform.position, _oppositionHalfFocusPoint, _focusSpeedMultiply);
+        }
+    }
+}
